d8b167c03cd4ec26073702487886075463ff7f60
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-05-26 23:20:58 -0400

    pickle autoloading good

--- ensemble.py
+++ ensemble.py
@@ -23,7 +23,7 @@ from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
@@ -53,35 +53,34 @@ class AutoEnsemble(object):
 
 
 	def feature_files(self):
-		trainfile = 'fixtures/train_features.csv'
+		trainfile = 'fixtures/train_features.pickle'
 		
 		if os.path.isfile(trainfile):
-			self.df = pd.read_csv(trainfile, index_col=0)
-			self.dfnum = self.df._get_numeric_data()
+			self.df = pd.read_pickle(trainfile)
 			self.y = self.df.OpenStatus
 		else:
 			self.df = self.make_features(self.trainfile)
 			self.openstatus_count = pd.DataFrame({'OpenStatus_Count' : self.df.groupby([u'OwnerUserId']).OpenStatus.sum()  }).reset_index()
-			self.df = pd.merge(self.df, self.openstatus_count, on=['OwnerUserId'])			
-
+			self.df = pd.merge(left=self.df, right=self.openstatus_count, how='left', left_on='OwnerUserId', right_on='OwnerUserId')			
+			self.df['OpenStatus_Count'] = self.df['OpenStatus_Count'].fillna(0)
 			self.y = self.df.OpenStatus
-			self.df.to_csv(trainfile)
+			self.df.to_pickle(trainfile)
 
 		
-		testfile =  'fixtures/tested_features.csv'
+		testfile =  'fixtures/tested_features.pickle'
 		if os.path.isfile(testfile):
-			self.X_test = pd.read_csv(testfile, index_col=0)
-			self.X_testnum = self.X_test._get_numeric_data()
+			self.X_test = pd.read_pickle(testfile)
 		else:
 			self.X_test = self.make_features(self.testfile)
 			self.openstatus_count = pd.DataFrame({'OpenStatus_Count' : self.df.groupby([u'OwnerUserId']).OpenStatus.sum()  }).reset_index()
-			self.X_test = pd.merge(self.X_test, self.openstatus_count, on=['OwnerUserId'])
-			self.X_test.to_csv(testfile)
+			self.X_test = pd.merge(left=self.X_test, right=self.openstatus_count, how='left', left_on='OwnerUserId', right_on='OwnerUserId')
+			self.X_test['OpenStatus_Count'] = self.X_test['OpenStatus_Count'].fillna(0)
+			self.X_test.to_pickle(testfile)
 
 		self.y_pred = [0 for elem in range(0, self.X_test.shape[0])]
 
 	def make_features(self, filename):
-		df = pd.read_csv(filename, index_col=0)
+		df = pd.read_csv(filename)
 		df.rename(columns={'OwnerUndeletedAnswerCountAtPostTime':'Answers'}, inplace=True)
 		df['TitleLength'] = df.Title.apply(len)
 		df['BodyLength'] = df.BodyMarkdown.apply(len)
@@ -99,12 +98,22 @@ class AutoEnsemble(object):
 
 		#df['CreatetoPostDateDelta'] = df['PostCreationDate'] - df['OwnerCreationDate']
 		#df['CreatetoPostDateDelta_seconds'] = df['CreatetoPostDateDelta'] / np.timedelta64(1, 's')
-		# openstatus_count = pd.DataFrame({'OpenStatus_Count' : df.groupby([u'OwnerUserId']).OpenStatus.sum()  }).reset_index()
-		# df = pd.merge(df, openstatus_count, on=['OwnerUserId'])
-		def first(x):
+
+		def clean_text(x):
 			text = bleach.clean(x, strip=True)
-			return text
-		df['Cleaned_BodyMarkdown'] = df.BodyMarkdown.apply(first)
+			return unicode(text)
+		df['Cleaned_BodyMarkdown'] = df.BodyMarkdown.apply(clean_text)
+
+		# def first(x):
+		# 	sent = nltk.sent_tokenize(x)
+		# 	return sent[0]
+		# df['First_Sentence'] = df.Cleaned_BodyMarkdown.apply(first)
+
+		# def last(x):
+		# 	sent = nltk.sent_tokenize(x)
+		# 	return sent[-1]
+		# df['Last_Sentence'] = df.Cleaned_BodyMarkdown.apply(last)
+
 
 
 
@@ -129,17 +138,17 @@ class AutoEnsemble(object):
 
 		probas = self.y_pred / self.count_models
 
-		sub = pd.DataFrame({'id':self.X_test.index, 'OpenStatus':probas}).set_index('id')
+		sub = pd.DataFrame({'id':self.X_test.PostId, 'OpenStatus':probas}).set_index('id')
 		sub.to_csv('sub.csv')
 
-	def pickle_loader(self, pick_file=None):
-		if os.path.isfile(pickle_file):
-			with open(pickle_file, "r") as fp: 	#Load model from file
-				grid.best_estimator_ = pickle.load(fp)
-				return grid.best_estimator_
+	def pickle_loader(self, pickle_file=None):
+		with open(pickle_file, "r") as fp: 	#Load model from file
+			grid = pickle.load(fp)
+			return grid
 
 	def pickler(self, model=None, name=None, feature_cols=None):
-		pickle_file = 'models/' + name + feature_cols + '_.pickle'
+		
+		pickle_file = 'models/' + name + ''.join(feature_cols) + '.pickle'
 		with open(pickle_file, "w+") as fp:
 			pickle.dump(model.best_estimator_, fp)
 
@@ -159,10 +168,87 @@ class AutoEnsemble(object):
 
 		self.logger(name=cname, score=grid.best_score_, best=grid.best_params_, tested_feature_cols=feature_cols)
 
+		self.pickler(model=grid, name=cname, feature_cols=feature_cols)
 
-		self.pickler(model=grid, name=cname, feature_cols='BodyMarkdown')
+		self.y_pred += grid.predict_proba(self.X_test[feature_cols])[:, 1]
+		self.count_models += 1
 
-		return grid.best_estimator_
+		print cname, feature_cols, grid.best_score_
+
+	def text_logisticregression_load(self, feature_cols=None):
+		cname = 'text_logisticregression'
+		pickle_file = 'models/' + cname + ''.join(feature_cols) + '.pickle'
+
+		if os.path.isfile(pickle_file):
+			grid = self.pickle_loader(pickle_file=pickle_file)
+			print 'pickle loaded'
+		else:
+
+			print 'pickle not loaded'
+			self.X = self.df[feature_cols]
+			
+			pipe = make_pipeline(CountVectorizer(ngram_range=(1, 1), stop_words='english'), 
+				LogisticRegression())
+
+			c_range = arange(.1, 1, .1)
+			p_options = ['l2', 'l1']
+			param_grid = dict(logisticregression__C=c_range, logisticregression__penalty=p_options)
+			grid = GridSearchCV(pipe, param_grid, cv=5, scoring='log_loss')
+			grid.fit(self.X, self.y)		
+
+			self.logger(name=cname, score=grid.best_score_, best=grid.best_params_, tested_feature_cols=feature_cols)
+
+			self.pickler(model=grid, name=cname, feature_cols=feature_cols)
+
+		self.y_pred += grid.predict_proba(self.X_test[feature_cols])[:, 1]
+		self.count_models += 1
+
+		print cname, feature_cols
+
+
+	def knn(self, feature_cols=None):
+		cname = 'knn'
+
+		pickle_file = 'models/' + cname + ''.join(feature_cols) + '.pickle'
+
+		self.X = self.df[feature_cols]
+		self.y_test = self.X_test[feature_cols]
+		scaler = StandardScaler()
+	
+		scaler.fit(self.X)
+		self.X = scaler.transform(self.X)
+		self.y_test = scaler.transform(self.y_test)
+
+
+		if os.path.isfile(pickle_file):
+			grid = self.pickle_loader(pickle_file=pickle_file)
+			print 'pickle loaded'
+
+		else:
+			print 'pickle not loaded'
+
+			neighbors_range = range(1, 100, 1)
+			weight_options = ['uniform', 'distance']
+			param_grid = dict(n_neighbors=neighbors_range, weights=weight_options)
+
+			knn = KNeighborsClassifier()
+
+			grid = GridSearchCV(knn, param_grid, cv=5, scoring='log_loss')
+			grid.fit(self.X, self.y)
+			
+
+			self.logger(name=cname, score=grid.best_score_, best=grid.best_params_, tested_feature_cols=feature_cols)
+
+			self.pickler(model=grid, name=cname, feature_cols=feature_cols)
+
+
+
+
+		self.y_pred += grid.predict_proba(self.y_test)[:, 1]
+		self.count_models += 1
+
+		print cname, feature_cols
+
 
 	def randomforest(self, feature_cols=None):
 		cname = 'randomforest'
@@ -182,16 +268,44 @@ class AutoEnsemble(object):
 
 		self.logger(name=cname, score=grid.best_score_, best=grid.best_params_, tested_feature_cols=feature_cols)
 
-		self.pickler(model=grid, name=cname, feature_cols='NumericData')
+		# self.pickler(model=grid, name=cname, feature_cols='NumericData')
 		
-	def randomforest_53():
-		feature_cols = ['ReputationAtPostCreation', 'OpenStatus_Count', 'Answers', 'TitleLength', 'BodyLength', 'NumTags']
-
-
-		grid = pickle_loader(pickle_file='53randomforestNumericData_.pickle')
-
-		self.y_pred += grid.predict_proba(self.X_test[feature_cols])[:, 1]
+		self.y_pred += grid.predict_proba(self.y)[:, 1]
 		self.count_models += 1
+
+	def randomforest_XXX(self):
+		feature_cols = ['ReputationAtPostCreation', 'OpenStatus_Count', 'Answers', 'TitleLength', 'BodyLength', 'NumTags']
+		self.X = self.df[feature_cols]
+
+
+		# grid = self.pickle_loader(pickle_file='models/randomforestNumericData53_.pickle')
+
+		RandomForestClassifier()
+
+		scaler = StandardScaler()
+		scaler.fit(self.X)
+		self.y = scaler.transform(self.X_test[feature_cols])
+
+
+		self.y_pred += grid.predict_proba(self.y)[:, 1]
+		self.count_models += 1
+
+	def randomforest_53(self):
+		feature_cols = ['ReputationAtPostCreation', 'OpenStatus_Count', 'Answers', 'TitleLength', 'BodyLength', 'NumTags']
+		self.X = self.df[feature_cols]
+
+		rfclf = RandomForestClassifier(n_estimators=240, max_features=3, oob_score=True, random_state=1)
+		
+		rfclf.fit(self.X, self.y)
+
+		self.y_pred += rfclf.predict_proba(self.X_test[feature_cols])[:, 1]
+		self.count_models += 1
+
+		print 'done'
+
+
+
+
 
 	def ensemble(self):
 		"""
@@ -218,11 +332,12 @@ class AutoEnsemble(object):
 
 	def main(self):
 		# Feature Columns to test ['Title', 'Tag1', 'FirstFourSentences']
-		# self.text_logisticregression(feature_cols='Tag1')
-		# self.text_logisticregression(feature_cols='Title')
-		# self.text_logisticregression(feature_cols='BodyMarkdown')
+		self.text_logisticregression_load(feature_cols='Title')
+		# self.text_logisticregression(feature_cols='Cleaned_BodyMarkdown')
+		# self.text_logisticregression(feature_cols='First_Sentence')
+		# self.text_logisticregression(feature_cols='Last_Sentence')
 
-		# self.randomforest(feature_cols=['ReputationAtPostCreation', 'OpenStatus_Count', 'Answers', 'TitleLength', 'BodyLength', 'NumTags'])
+		self.knn(feature_cols=['Answers', 'ReputationAtPostCreation'])
 
 		self.submission()
 
